5f2f18fe6d8af3d1ebf8d292b63219967e5b7b96
hochimano <jastrathy@hotmail.com>
2025-02-17 12:00:16 -0800

    Fixed DPR!

--- Skibs.py
+++ Skibs.py
@@ -9,7 +9,6 @@ import sys
 
 
 class team:
-
     def __init__(self, number):
         self._matchDPR = []
         self._number = number
@@ -19,7 +18,7 @@ class team:
         return self._number         
     
     def addMatch(self, DPR):
-        self._matchDPR.append(DPR)
+        self._matchDPR.append(DPR) 
         self._totalDPR += DPR
 
     def getSDV(self):
@@ -27,39 +26,51 @@ class team:
          
 
 def getMatchDPR(match, color):
+
+    sum = 0
     #TODO: make sure games that dont exist are added
     if color:
-        if (match["blue_teleop_epa_sum"] != None) & (match["blue_teleop"] != None):
-            return match["blue_teleop_epa_sum"] - match["blue_teleop"]
-        else: return match["blue_epa_sum"] - match["blue_score"]
+        for t in match['alliances']['blue']['team_keys']:
+            sum += teams[t]['epa']['breakdown']['teleop_points'] 
+        return sum - match['result']["blue_teleop_points"]
     else:
-        if (match["red_teleop_epa_sum"] != None) & (match["red_teleop"] != None):
-            return match["red_teleop_epa_sum"] - match["red_teleop"]
-        else: return match["red_epa_sum"] - match["red_score"]
+        for t in match['alliances']['red']['team_keys']:
+            sum += teams[t]['epa']['breakdown']['teleop_points'] 
+        return sum - match['result']["red_teleop_points"]
 
 
-file_path = 'DataFrame.xlsx'
+
+
+
+file_path = 'Stats/DataFrame.xlsx'
 workBook = load_workbook(file_path)
-sheet_names = workBook.sheetnames
 
 # Delete all sheets
-for sheet_name in sheet_names:
+for sheet_name in workBook.sheetnames:
     del workBook[sheet_name]
 
 # Save the workbook
 sb = statbotics.Statbotics()
 
-#ONLY CHANGE THIS
+
 
 while(True):
     _event = input("Enter Comp: ")
     if _event == "quit": break
-    workBook.create_sheet(title=_event)
     print(_event)
 
+    #this query is currently broken and cannot be used
+    #for now I will get a list of teams from the matches
+    #TODO: uncomment this when it is fixed
+    #teams = sb.get_team_events(event = _event)
 
-    teams = sb.get_team_events(event = _event)
-    matches = sb.get_matches(event = _event)
+    #matches = sb.get_matches(event = _event)
+    matches = sb.get_matches(event = _event, limit = 1000)
+    Tempteams = sb.get_team_events(event = _event, limit = 1000)
+    teams = {}
+
+    for t in Tempteams:
+        teams[t['team']] = t
   
 
     teamDict = {}
@@ -75,10 +86,13 @@ while(True):
     matchDPRArray = np.zeros(len(matches)*2)
 
     tempTeamNum = 0
-    for t in teams:
-        cur = team(t["team"])
-        SDVDict[t["team"]] = cur
-        teamDict[t["team"]] = tempTeamNum
+
+    #this is changed from t["team"] to just t for the workaround
+    #TODO: change this back when the API gets fixed
+    for t in Tempteams:
+        cur = team(t['team'])
+        SDVDict[t['team']] = cur
+        teamDict[t['team']] = tempTeamNum
         tempTeamNum += 1
 
 
@@ -88,27 +102,27 @@ while(True):
 
 
     #print(sb.get_team_events(team = 5827, year = 2024))
+        #could have broken something here
     matchCount = 0
     for m in matches:
-        if (type(m["red_score"]) == int or type(m["red_score"]) == float):
-            matchArray[matchCount][teamDict[m["red_1"]]] = 1
-            matchArray[matchCount][teamDict[m["red_2"]]] = 1
-            matchArray[matchCount][teamDict[m["red_3"]]] = 1
-            SDVDict[m["red_1"]].addMatch(getMatchDPR(m, True))
-            SDVDict[m["red_2"]].addMatch(getMatchDPR(m, True))
-            SDVDict[m["red_3"]].addMatch(getMatchDPR(m, True))
-            matchDPRArray[matchCount] = getMatchDPR(m, True)
+        if m['result']['red_teleop_points'] != None and m['result']['blue_teleop_points'] != None:
+            redDPR = getMatchDPR(m,True)
 
+            for t in m['alliances']['red']['team_keys']:
+                matchArray[matchCount][teamDict[t]] = 1
+                SDVDict[t].addMatch(getMatchDPR(m, True))
+
+            matchDPRArray[matchCount] = getMatchDPR(m, True)
             matchCount += 1
 
-            matchArray[matchCount][teamDict[m["blue_1"]]] = 1
-            matchArray[matchCount][teamDict[m["blue_2"]]] = 1
-            matchArray[matchCount][teamDict[m["blue_3"]]] = 1
-            SDVDict[m["blue_1"]].addMatch(getMatchDPR(m, False))
-            SDVDict[m["blue_2"]].addMatch(getMatchDPR(m, False))
-            SDVDict[m["blue_3"]].addMatch(getMatchDPR(m, False))
-            matchDPRArray[matchCount] = getMatchDPR(m, False)
+            blueDPR = getMatchDPR(m,False)
 
+            for t in m['alliances']['blue']['team_keys']:
+                matchArray[matchCount][teamDict[t]] = 1
+                SDVDict[t].addMatch(getMatchDPR(m, False))
+
+                #not sure whether this line goes in the loop or not
+            matchDPRArray[matchCount] = getMatchDPR(m, False)
             matchCount += 1
 
     #DO NOT TOCUH PLEASE
@@ -128,13 +142,15 @@ while(True):
 
     #matchNorm = np.dot(np.transpose(matchArray), matchArray)
     #DPRNorm = np.dot(np.transpose(matchArray), matchDPRArray)
-
+        
+    #like all other changes this was made to workaround get_teams_events being broken chagne back when it is fixed
+    #TODO: fix this later
     for t in teams:
         #dataOut({t["team"], dprs[0][teamDict[t["team"]]]})
-        tempDict['EPA'].append(sb.get_team_event(team = t["team"], event = _event)['epa_mean'])
-        tempDict["Number"].append(t["team"])
-        tempDict["DPR"].append(round((dprs[0][teamDict[t["team"]]] + abs(floor)) / (abs(floor) + ceiling),2))
-        tempDict["SDV"].append(SDVDict[t["team"]].getSDV())
+        tempDict['EPA'].append(teams[t]['epa']['breakdown']['total_points'])
+        tempDict["Number"].append(t)
+        tempDict["DPR"].append(round((dprs[0][teamDict[t]] + abs(floor)) / (abs(floor) + ceiling),2))
+        tempDict["SDV"].append(SDVDict[t].getSDV())
         #tempDict["Name"].append(name_dict[t["team"]])
         
 
@@ -148,7 +164,12 @@ while(True):
     sorted_df = df.sort_values(by='DPR', ascending=False)
     pd.set_option('display.max_rows', None)
     print(tabulate(sorted_df, headers = 'keys', tablefmt = 'psql', showindex=False))
-    df.to_excel('DataFrame.xlsx', index=False, sheet_name = _event)
+
+    new_sheet = workBook.create_sheet(title=_event)
+    workBook.save(file_path)
+
+    df.to_excel(file_path, index=False, sheet_name=_event)
+    print(workBook.sheetnames)
 
 
 
